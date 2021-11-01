cf17e19dfc6d06a2535a1b196d24f93a2c5b74b2
yixian185 <54654958+yixian185@users.noreply.github.com>
2021-11-01 21:30:01 +0800

    Add files via upload

---
+++ liyixian/R/xinan.R
@@ -0,0 +1,15 @@
+#' xinan function
+#' This function allow you to calculate the multiplication between two numbers.
+#' The fomula used in the function is simple.
+#' @param x enter the number, eg. 2
+#' @param y enter the number, eg. 5
+#' @keywords xinan
+#' @export
+#' @examples
+#' xinan(2,5)
+#'
+xinan<-function(x,y){
+  z=x*y
+}
+
+
